9eebf15edefa2b05287931200b695caca71be279
Antonio Sartori <antoniosartori@chromium.org>
2022-02-18 03:00:49 -0800

    [CSP] Enhance unsafe-eval test to check both realms
    When checking whether eval is allowed, CSPs of both callerRealm and
    calleeRealm should be checked.
    
    Change-Id: I89d3f3f2352dc63538b8479b058f44c12e9ede1a

--- content-security-policy/unsafe-eval/support/echo-eval-with-policy.py
+++ content-security-policy/unsafe-eval/support/echo-eval-with-policy.py
@@ -5,10 +5,36 @@ def main(request, response):
 <html>
 <script>
 var id = 0;
+var evalInIframe = "allowed";
 try {
   id = eval("id + 1");
-} catch (e) {}
-window.parent.postMessage(id === 1 ? "eval allowed" : "eval blocked");
+} catch (e) {
+  if (e instanceof EvalError) {
+    if (id === 0)
+      evalInIframe = "blocked";
+    else
+      evalInIframe = "EvalError exception, but eval was executed";
+  } else {
+    evalInIframe = "Unexpected exception: " + e.message;
+  }
+}
+
+id = 0;
+var evalInParent = "allowed";
+try {
+  id = parent.eval("1 + 1");
+} catch (e) {
+  if (e instanceof parent.EvalError) {
+    if (id === 0)
+      evalInParent = "blocked";
+    else
+      evalInParent = "EvalError exception, but eval was executed";
+  } else {
+    evalInParent = "Unexpected exception: " + e.message;
+  }
+}
+
+window.parent.postMessage({ evalInParent: evalInParent, evalInIframe: evalInIframe});
 </script>
 </html>
 """
